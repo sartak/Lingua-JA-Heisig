449ceb12e1e75a5e7b8407f0a2d1fbf692af300b
Shawn M Moore <sartak@gmail.com>
2009-12-01 21:01:09 -0500

    Boilerplate

---
+++ lib/Lingua/JA/Heisig.pm
@@ -0,0 +1,27 @@
+package Lingua::JA::Heisig;
+use strict;
+use warnings;
+use utf8;
+our $VERSION = 0.01;
+
+1;
+
+=head1 NAME
+
+Lingua::JA::Heisig - Utility functions for Heisig's Remembering the Kanji 
+
+=head1 SYNOPSIS
+
+
+=head1 AUTHOR
+
+Shawn M Moore, C<sartak@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
