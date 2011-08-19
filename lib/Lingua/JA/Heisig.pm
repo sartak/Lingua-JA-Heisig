bd687d4c9a3a66e370c610ec9f8abe0845de7729
Shawn M Moore <sartak@gmail.com>
2011-08-19 14:33:08 -0400

    2011 copyright

--- lib/Lingua/JA/Heisig.pm
+++ lib/Lingua/JA/Heisig.pm
@@ -167,7 +167,7 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2009 Shawn M Moore.
+Copyright 2009-2011 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
